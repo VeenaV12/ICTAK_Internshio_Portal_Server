e453609523c81fc7a5a115f4f6445a82beca2fa4
Veena Vidyanandan <106391333+VeenaV12@users.noreply.github.com>
2024-08-05 21:01:05 +0530

    change in the pathfile

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use('/', forumRouter)
 
 app.use('/', submitRouter);
 
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
